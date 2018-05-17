3af1536083d7fab7dc4ab8a6531e5e3e445e6c4b
mackenziemarques97 <38563099+mackenziemarques97@users.noreply.github.com>
2018-05-17 13:11:55 -0400

    testing

--- Demonstration.m
+++ Demonstration.m
@@ -1,17 +1,16 @@
-a = setUpSerial('COM5');
-while(1)
-    saccade(a,50,50,10000);
-    smoothpursuit(a,50,90,50,10,10,30,2);
-    saccade(a,75,50,10000);
-    calibrate(a);
-    
-    saccade(a,50,50,10000);
-    smoothpursuit(a,10,50,90,50,10,30,2);
-    calibrate(a);
-    smoothpursuit(a,10,10,90,90,10,30,2);
-    saccade(a,100,100,10000);
-    calibrate(a);
-end
+a = setUpSerial('COM7');
+
+saccade(a,50,50,10000);
+smoothpursuit(a,50,90,50,10,10,30,2);
+% saccade(a,75,50,10000);
+calibrate(a);
+
+% saccade(a,50,50,10000);
+% smoothpursuit(a,10,50,90,50,10,30,2);
+% calibrate(a);
+% smoothpursuit(a,10,10,90,90,10,30,2);
+% saccade(a,100,100,10000);
+% calibrate(a);
 fclose(a);
 
 function smoothpursuit(connection,x0,y0,x1,y1,speed,res,rep)
