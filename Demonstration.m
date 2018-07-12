9068df7b06420343e7223b7f34688a0dfc3d25f9
Josh Wu <wu.joshuah@gmail.com>
2018-07-12 12:46:03 -0400

    Revert "Merge branch 'development' of https://github.com/joshuahwu/VergenceRobot into development"
    This reverts commit dd334200d94968a21aa1fc00c28bde55d0ccb154, reversing
    changes made to f38726ccaf292bc94ff289ffe78be1b812bbf38e.

--- Demonstration.m
+++ Demonstration.m
@@ -1,22 +1,22 @@
 a = setUpSerial('COM7');
 
 saccade(a,50,50,10000);
-oscillate(a,50,90,50,10,10,30,2);
+smoothpursuit(a,50,90,50,10,10,30,2);
 % saccade(a,75,50,10000);
 calibrate(a);
 
 % saccade(a,50,50,10000);
-% oscillate(a,10,50,90,50,10,30,2);
+% smoothpursuit(a,10,50,90,50,10,30,2);
 % calibrate(a);
-% oscillate(a,10,10,90,90,10,30,2);
+% smoothpursuit(a,10,10,90,90,10,30,2);
 % saccade(a,100,100,10000);
 % calibrate(a);
 fclose(a);
 
-function oscillate(connection,x0,y0,x1,y1,speed,res,rep)
-fprintf(connection,['Oscillate:%d:%d:%d:%d:%d:%d:%d'],[x0,y0,x1,y1,speed,res,rep]);
+function smoothpursuit(connection,x0,y0,x1,y1,speed,res,rep)
+fprintf(connection,['Smooth:%d:%d:%d:%d:%d:%d:%d'],[x0,y0,x1,y1,speed,res,rep])
 while(strcmp(fscanf(connection,'%s'),'Done')~=1)
-    disp('Waiting Oscillation Trial')
+    disp('Waiting Smooth Pursuit Trial')
 end
 end
 
@@ -33,12 +33,5 @@ function saccade(connection,x,y,delay)
 fprintf(connection,['Saccade:%d:%d:%d:'],[x,y,delay]);
 while(strcmp(fscanf(connection,'%s'),'Done')~=1)
     disp('Waiting Saccade Trial')
-end
-end
-
-function arc(connection,radius,angInit,angFinal,speed,res)
-fprintf(connection,['Arc:%d:%d:%d:%d:%d:'],[radius,angInit,angFinal,speed,res]);
-while(strcmp(fscanf(connection,'%s'),'Done')~=1)
-    disp('Waiting Arc Trial')
 end
 end
